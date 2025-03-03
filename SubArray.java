bd10945ffe7022356b1dbdc2949dc8dcb4d87580
GowdaHemannth <hemanthagalahalli@gmail.com>
2025-03-03 18:58:32 +0530

    SubArray.java

---
+++ SubArray.java
@@ -0,0 +1,14 @@
+public class SubArray {
+    public static void main(String[] args) {
+        int arr[]={2,1,4,9};
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+           for(int j=i+2;j<arr.length;j++){
+           max=Math.max(max,arr[i]+arr[j]);
+            }
+        }
+        System.out.println(max);
+    }
+    
+}
