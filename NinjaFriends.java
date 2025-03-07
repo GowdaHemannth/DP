0de0f9d86d832ed4cb7897cb6e2368ca26116c99
GowdaHemannth <hemanthagalahalli@gmail.com>
2025-03-07 08:54:12 +0530

    SubSet.java

---
+++ NinjaFriends.java
@@ -0,0 +1,42 @@
+public class NinjaFriends {
+    public static int Function(int i,int j1,int j2,int matrix[][],int n,int m){
+        // Base Case Or Out of Bound Case 
+        if(j1<0||j1>m||j2<0||j2>m){
+           return  Integer.MIN_VALUE;
+        }
+        // Base Case If it reaches Lat row
+        if(i==n){
+            if(j1==j2){
+                return matrix[i][j1];
+            }else{
+                return matrix[i][j1]+matrix[i][j2];
+            }
+        }
+        int Max=Integer.MIN_VALUE;
+
+        for(int di=-1;di<=1;di++){
+            for(int dj=-1;dj<=1;dj++){
+                int ans;
+                if(j1==j2){
+                    ans=matrix[i][j1]+Function(i+1, j1+di, j2+dj, matrix, n ,m);
+                }else{
+                    ans=matrix[i][j1]+matrix[i][j2]+Function(i+1, j1+di, j2+dj, matrix, n,m);
+                }
+                Max=Math.max(Max, ans);
+            }
+        }
+        return Max;
+    }
+    public static void main(String[] args) {
+        int matrix[][] =  {  
+            {3, 1, 1},  
+            {2, 5, 1},  
+            {1, 5, 5},  
+            {2, 1, 1}  
+        };
+    int n = matrix.length-1;
+    int m = matrix[0].length-1;
+System.out.println(Function(0, 0, m, matrix, n, m));
+    }
+    
+}
---
+++ SubSet.java
@@ -0,0 +1,26 @@
+public class SubSet {
+    public static boolean Function(int index,int arr[],int target){
+        if(target==0){
+            return true;
+        }
+        if(index==0){
+            return arr[0]==target; 
+        }
+        boolean Take=false;
+        if(arr[index]<=target){
+            Take=Function(index-1, arr, target-arr[index]);
+
+        }
+        boolean NotTake=Function(index-1, arr, target);
+
+        return Take||NotTake;
+    }
+    public static void main(String[] args) {
+        int arr[]={2,3,1,1};
+        int n=arr.length-1;
+        int target=10;
+      
+        System.out.println(Function(n, arr, target));
+    }
+    
+}
